238ab9b5429af9bfc96a1f3e4d6628a38a7ae933
AmyXandra <amyabafor013@gmail.com>
2021-06-16 12:41:20 +0100

    added the toggle icons and updated headers

--- js/theme.js
+++ js/theme.js
@@ -1,30 +1,25 @@
 // Select the button
 const modeBtn = document.getElementById('mode-toggle');
-console.log("modeBtn", modeBtn)
-
-// modeBtn.addEventListener('click', function() {
-//   console.log('button clicked');    
-// });
-
+// const modeBtn = document.querySelector('#mode-toggle');
+// console.log("modeBtn", modeBtn)
 
 
 const currentTheme = localStorage.getItem("theme");
-console.log("currentTheme",currentTheme)
+// console.log("currentTheme",currentTheme)
 if (currentTheme == "dark") {
   document.body.classList.add("dark-theme");
 }
 
 modeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
-  console.log('button clicked');    
+  // console.log('button clicked');    
   let theme = "light";
   if (document.body.classList.contains("dark-theme")) {
     theme = "dark";
   }
   localStorage.setItem("theme", theme);
-console.log("theme",theme)
+// console.log("theme",theme)
   
 });
 
 
-Resources
